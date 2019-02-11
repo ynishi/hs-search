4d1589ac1cde4bd28536f04a605d0849843a94a9
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-02-11 10:42:37 +0900

    use GADTs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,37 +1,120 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
-data Bit = Bit String deriving (Show)
+module Lib where
+
+import           Data.List
+
+data Bit =
+  Bit String
+  deriving (Show)
 
 type MaxN = Int
+
 type MinN = Int
 
-data Match = Match Bit | Ngram MaxN MinN Bit | Keyword Bit deriving (Show)
+data Match
+  = Match Bit
+  | Ngram MaxN
+          MinN
+          Bit
+  | Keyword Bit
+  deriving (Show)
+
+matchToQueryStr :: Match -> String
+matchToQueryStr (Match (Bit s)) = "{\"match\": \"" ++ s ++ "\"}"
+matchToQueryStr (Ngram _ _ (Bit s)) =
+  "{\"match\": \"{\"query\":" ++ s ++ "\",\"analyzer\":\"ngram\"}"
+matchToQueryStr (Keyword (Bit s)) = "{\"term\": \"" ++ s ++ "\"}"
 
 type Stem = Bit
-data Intent = Intent Stem [Match] deriving (Show)
 
-data Coef = Coef Float [Intent] | InvCoef Float [Intent] deriving (Show)
+data Intent =
+  Intent Stem
+         [Match]
+  deriving (Show)
 
-data Expr a = Expr a | And (Expr a) (Expr a) | Not (Expr a) | Or (Expr a) (Expr a) deriving(Show)
+data Query a where
+  Query :: Intent -> Query Intent
+  AndQ :: Query a -> Query a -> Query a
+  OrQ :: Query a -> Query a -> Query a
+  NotQ :: Query a -> Query a
+  ListQ :: [Intent] -> Query [Intent]
 
-data Search = Search [Coef] deriving (Show)
+deriving instance Show a => Show (Query a)
+
+eval :: Query a -> String
+eval (Query (Intent _ ms)) =
+  intercalate "," $ map (\m -> "{" ++ (matchToQueryStr m) ++ "}") ms
+eval (AndQ x y) = "{\"bool\":{\"must\":[" ++ (eval x) ++ "," ++ (eval y) ++ "]}"
+eval (NotQ x) = "{\"bool\": {\"must_not\":" ++ (eval x) ++ "}"
+eval (OrQ x y) =
+  "{\"bool\":{\"should\":[" ++ (eval x) ++ "," ++ (eval y) ++ "]}"
+eval (ListQ xs) =
+  "[" ++ (intercalate ", " $ map (\x -> eval (Query x)) xs) ++ "]"
+
+data Coef
+  = Coef Float
+         [Intent]
+  | InvCoef Float
+            [Intent]
+  deriving (Show)
+
+data Expr a
+  = Expr a
+  | And (Expr a)
+        (Expr a)
+  | Not (Expr a)
+  | Or (Expr a)
+       (Expr a)
+  deriving (Show)
+
+data Search =
+  Search [Coef]
+  deriving (Show)
 
 and :: Coef -> Coef -> Coef
-and (Coef f1 i1) (Coef f2 i2) = Coef (f1 * f2) (i1 ++ i2) 
+and (Coef f1 i1) (Coef f2 i2)       = Coef (f1 * f2) (i1 ++ i2)
 and (InvCoef f1 i1) (InvCoef f2 i2) = InvCoef (f1 * f2) (i1 ++ i2)
 
 someFunc :: IO ()
 someFunc = do
-    putStrLn $ show c3 
-    where
-      b1 = Bit "ab"
-      b2 = Bit "abc"
-      m1 = Match b1
-      m2 = Ngram 2 3 b2 
-      i1 = Intent b1 [m1]
-      i2 = Intent b2 [m2]
-      c1 = Coef 1.0 [i1]
-      c2 = Coef 1.0 [i2]
-      c3 = Lib.and c1 c2
+  putStrLn $ show c3
+  where
+    b1 = Bit "ab"
+    b2 = Bit "abc"
+    m1 = Match b1
+    m2 = Ngram 2 3 b2
+    i1 = Intent b1 [m1]
+    i2 = Intent b2 [m2]
+    c1 = Coef 1.0 [i1]
+    c2 = Coef 1.0 [i2]
+    c3 = Lib.and c1 c2
+
+b1 = Bit "ab"
+
+b2 = Bit "abc"
+
+m1 = Match b1
+
+m2 = Ngram 2 3 b2
+
+i1 = Intent b1 [m1]
+
+i2 = Intent b2 [m2]
+
+q1 = Query i1
+
+q2 = Query i2
+
+q2' = Query (Intent (Bit "abcd") [Ngram 4 4 (Bit "abcd")])
+
+q3 = AndQ q1 q2
+
+q4 = OrQ (NotQ q1) (AndQ q2 q2')
+
+c1 = Coef 1.0 [i1]
+
+c2 = Coef 1.0 [i2]
+
+c3 = Lib.and c1 c2
